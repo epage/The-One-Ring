ad85e0608ea242356672fc011fe92ee3effb89e6
Ed Page <eopage@byu.net>
2009-09-26 19:02:25 -0500

    My telepathy doesn't support it yet but giving myself a reminder to later implement location aware-ness using the area codes

---
+++ src/location.py
@@ -0,0 +1,25 @@
+import telepathy
+
+
+class TheOneRingLocation(telepathy.server.ConnectionInterfaceLocation):
+
+	def __init__(self):
+		telepathy.server.ConnectionInterfaceLocation.__init__(self)
+
+	def GetLocations(self, contacts):
+		"""
+		@returns {Contact: {Location Type: Location}}
+		"""
+		raise NotImplementedError()
+
+	def RequestLocation(self, contact):
+		"""
+		@returns {Location Type: Location}
+		"""
+		raise NotImplementedError()
+
+	def SetLocation(self, location):
+		"""
+		Since presence is based off of phone numbers, not allowing the client to change it
+		"""
+		raise telepathy.errors.PermissionDenied()
