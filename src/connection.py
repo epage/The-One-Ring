243f514ed4cc40f72d148f8795413c7a527fc7d2
Ed Page <eopage@byu.net>
2010-01-09 19:05:50 -0600

    Temporarily disabling the Contacts Interface because it causes massive failure

--- src/connection.py
+++ src/connection.py
@@ -42,7 +42,7 @@ _moduleLogger = logging.getLogger("connection")
 class TheOneRingConnection(
 	telepathy.server.Connection,
 	telepathy.server.ConnectionInterfaceRequests, # already a mixin
-	contacts.ContactsMixin,
+	#contacts.ContactsMixin,
 	aliasing.AliasingMixin,
 	simple_presence.SimplePresenceMixin,
 	presence.PresenceMixin,
@@ -80,7 +80,7 @@ class TheOneRingConnection(
 			constants._telepathy_implementation_name_
 		)
 		telepathy.server.ConnectionInterfaceRequests.__init__(self)
-		contacts.ContactsMixin.__init__(self)
+		#contacts.ContactsMixin.__init__(self)
 		aliasing.AliasingMixin.__init__(self)
 		simple_presence.SimplePresenceMixin.__init__(self)
 		presence.PresenceMixin.__init__(self)
