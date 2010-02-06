b1549f16a69c5907bf216fb840de15dd2a3f1621
Ed Page <eopage@byu.net>
2010-02-06 16:27:34 -0600

    Some debugging helps for situations users are in

--- hand_tests/abook.py
+++ hand_tests/abook.py
@@ -3,12 +3,13 @@
 import sys
 sys.path.insert(0,"../src")
 import logging
+import pprint
 
 import gvoice.backend as backend
 import gvoice.addressbook as abook
 
 
-def main():
+def print_contacts():
 	logging.basicConfig(level=logging.DEBUG)
 
 	args = sys.argv
@@ -22,9 +23,30 @@ def main():
 	book = abook.Addressbook(b)
 	book.update()
 	for number in book.get_numbers():
-		print number, book.get_contact_name(number), book.get_phone_type(number)
+		#pprint.pprint((number, book.get_contact_name(number), book.get_phone_type(number), book.is_blocked(number)))
+		pprint.pprint(book._numbers[number])
+
+
+def print_blank_names():
+	logging.basicConfig(level=logging.DEBUG)
+
+	args = sys.argv
+	username = args[1]
+	password = args[2]
+
+	b = backend.GVoiceBackend()
+	b.login(username, password)
+	assert b.is_authed()
+
+	book = abook.Addressbook(b)
+	book.update()
+	for number in book.get_numbers():
+		if not book.get_contact_name(number):
+			pprint.pprint(book._numbers[number])
 
 
 if __name__ == "__main__":
-	main()
-
+	if True:
+		print_contacts()
+	else:
+		print_blank_names()
