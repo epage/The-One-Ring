21409ee55813e0c4c3af7eaa581789317a30dece
Ed Page <eopage@byu.net>
2010-04-03 12:05:07 -0500

    Bump to 0.8.9-1

--- src/constants.py
+++ src/constants.py
@@ -2,8 +2,8 @@ import os
 
 __pretty_app_name__ = "Telepathy-TheOneRing"
 __app_name__ = "telepathy-theonering"
-__version__ = "0.8.8"
-__build__ = 0
+__version__ = "0.8.9"
+__build__ = 1
 __app_magic__ = 0xdeadbeef
 _data_path_ = os.path.join(os.path.expanduser("~"), ".telepathy-theonering")
 _user_settings_ = "%s/settings.ini" % _data_path_
